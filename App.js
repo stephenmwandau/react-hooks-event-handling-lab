d8e4188f56bb0e06a889970bbafa849dbd2f6efd
stephenmwandau <stephen.mwandau@student.moringaschool.com>
2024-08-05 15:07:24 +0300

    comlpeted

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Keypad from './Keypad';
+import EyesOnMe from './EyesOnMe'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Keypad Component</h1>
+      <Keypad />
+      <EyesOnMe />
+    </div>
+  );
+}
+
+export default App;
+ 
--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+// Code EyesOnMe Component Here
+function EyesOnMe() {
+    function handleFocus(event) {
+        console.log ('Good!')
+    }
+    function handleBlur(event) {
+        console.log('Hey! Eyes on me!')
+    }
+    return(
+        <>
+            <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+        </>
+    );
+}
+export default EyesOnMe;
